2c78a43abc851cd9eee5f707facd81afe5db5971
83n38 <83nhendo@gmail.com>
2018-09-01 12:29:34 +1000

    Initial Commit

---
+++ MTRN2500_OpenGL_assign2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MTRN2500_OpenGL_assign2
+//
+//  Created by Ben Henderson on 1/9/18.
+//  Copyright © 2018 Ben Henderson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
