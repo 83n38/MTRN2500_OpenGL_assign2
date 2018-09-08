57ee5f6f81e158fd1176f3deb66614660fcf4317
83n38 <83nhendo@gmail.com>
2018-09-08 14:40:42 +1000

    car is moving and steering, although the pieces rotate about their own centre instead of the cars

--- MTRN2500_OpenGL_assign2/SnowPlowVehicle.cpp
+++ MTRN2500_OpenGL_assign2/SnowPlowVehicle.cpp
@@ -14,22 +14,25 @@ SnowPlowVehicle::SnowPlowVehicle() {
     float v2[] = {0.f, 3.5f, -2.f};
     float v3[] = {-2.f, 3.5f, -2.f};
     float v4[] = {-3.f, 2.f, -2.f};
-    TrapezoidalPrism* window = new TrapezoidalPrism(v1, v2, v3, v4, 4);
+    window = new TrapezoidalPrism(v1, v2, v3, v4, 4);
     
     //position for wheels
-    Cylinder* LRwheel = new Cylinder(-3.0, 0.5, -2.25, 1, 0.5);
-    Cylinder* RRwheel = new Cylinder(-3.0, 0.5, 2.25, 1, 0.5);
-    Cylinder* LFwheel = new Cylinder(3.0, 0.25, -2.25, 0.75, 0.5);
-    Cylinder* RFwheel = new Cylinder(3.0, 0.25, 2.25, 0.75, 0.5);
+    LRwheel = new Cylinder(-3.0, 0.5, -2.25, 1, 0.5);
+    RRwheel = new Cylinder(-3.0, 0.5, 2.25, 1, 0.5);
+    LFwheel = new Cylinder(3.0, 0.25, -2.25, 0.75, 0.5);
+    RFwheel = new Cylinder(3.0, 0.25, 2.25, 0.75, 0.5);
     
     //position for vehicle body
-    RectangularPrism* body = new RectangularPrism(0, 1, 0, 6, 2, 4);
+    body = new RectangularPrism(0, 1, 0, 6, 2, 4);
     
     //position for front diffuser
     float w1[] = {3.f, 0.f, -2.f};
     float w2[] = {3.f, 1.f, -2.f};
     float w3[] = {5.f, 0.f, -2.f};
-    TriangularPrism* frontdiffuser = new TriangularPrism(w1, w2, w3, 4);
+    frontdiffuser = new TriangularPrism(w1, w2, w3, 4);
+    
+    // get the offset from the vehicles X, Y, and Z
+    this->getOffsets();
     
     body->draw();
     frontdiffuser->draw();
@@ -49,9 +52,29 @@ SnowPlowVehicle::SnowPlowVehicle() {
 }
 
 void SnowPlowVehicle::draw() {
+    
+    //
+    
+    // set components the correct positions
+    window->setPosition(windowX + this->x, windowY + this->y, windowZ + this->z);
+    body->setPosition(bodyX + this->x, bodyY + this->y, bodyZ + this->z);
+    frontdiffuser->setPosition(frontdiffuserX + this->x, frontdiffuserY + this->y, frontdiffuserZ + this->z);
+    LRwheel->setPosition(xRwheelX + this->x, xRwheelY + this->y, LxwheelZ + this->z);
+    RRwheel->setPosition(xRwheelX + this->x, xRwheelY + this->y, RxwheelZ + this->z);
+    LFwheel->setPosition(xFwheelX + this->x, xFwheelY + this->y, LxwheelZ + this->z);
+    RFwheel->setPosition(xFwheelX + this->x, xFwheelY + this->y, RxwheelZ + this->z);
+    
+    // all components to the correct rotation
     std::vector<Shape *>::iterator it;
-    for (it = shapes.begin(); it != shapes.end(); ++it)
-    {
+    for (it = shapes.begin(); it != shapes.end(); ++it) {
+        (*it)->setRotation(this->rotation);
+    }
+    
+    // add steering
+    LFwheel->setRotation(LFwheel->getRotation() + steering);
+    RFwheel->setRotation(RFwheel->getRotation() + steering);
+    
+    for (it = shapes.begin(); it != shapes.end(); ++it) {
         (*it)->draw();
     }
     //    for (int i = 0; i < 4; i++) {
@@ -62,5 +85,30 @@ void SnowPlowVehicle::draw() {
     //    shaft.draw();
     
     
+}
+
+void SnowPlowVehicle::getOffsets() {
+    // window offsets
+    windowX = window->getX();
+    windowY = window->getY();
+    windowZ = window->getZ();
+    
+    // wheel offsets
+    xRwheelX = LRwheel->getX();
+    xRwheelY = LRwheel->getY();
+    xFwheelX = RFwheel->getX();
+    xFwheelY = RFwheel->getY();
+    LxwheelZ = LRwheel->getZ();
+    RxwheelZ = RFwheel->getZ();
+    
+    // body offsets
+    bodyX = body->getX();
+    bodyY = body->getY();
+    bodyZ = body->getZ();
+    
+    // diffuser offsets
+    frontdiffuserX = frontdiffuser->getX();
+    frontdiffuserY = frontdiffuser->getY();
+    frontdiffuserZ = frontdiffuser->getZ();
 }
 
--- MTRN2500_OpenGL_assign2/SnowPlowVehicle.hpp
+++ MTRN2500_OpenGL_assign2/SnowPlowVehicle.hpp
@@ -20,6 +20,40 @@ public:
     SnowPlowVehicle();
     
     void draw();
+private:
+    // shapes
+    TrapezoidalPrism* window;
+    RectangularPrism* body;
+    TriangularPrism* frontdiffuser;
+    Cylinder* LRwheel;
+    Cylinder* RRwheel;
+    Cylinder* LFwheel;
+    Cylinder* RFwheel;
+    
+    // window offsets
+    double windowX;
+    double windowY;
+    double windowZ;
+    
+    // wheel offsets
+    double xRwheelX;
+    double xRwheelY;
+    double xFwheelX;
+    double xFwheelY;
+    double LxwheelZ;
+    double RxwheelZ;
+    
+    // body offsets
+    double bodyX;
+    double bodyY;
+    double bodyZ;
+    
+    // diffuser offsets
+    double frontdiffuserX;
+    double frontdiffuserY;
+    double frontdiffuserZ;
+    
+    void getOffsets();
 };
 
 #endif /* SnowPlowVehicle_hpp */
