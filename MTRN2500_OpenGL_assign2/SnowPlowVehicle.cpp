34d318c88988a7f6eb16ab59190beec35f0aa000
83n38 <83nhendo@gmail.com>
2018-09-08 16:59:24 +1000

    made the rims rotate with steering

--- MTRN2500_OpenGL_assign2/SnowPlowVehicle.cpp
+++ MTRN2500_OpenGL_assign2/SnowPlowVehicle.cpp
@@ -8,6 +8,7 @@
 #include "SnowPlowVehicle.hpp"
 
 #include <iterator>
+#include <math.h>
 
 SnowPlowVehicle::SnowPlowVehicle() {
     float v1[] = {1.5f, 2.f, -2.f};
@@ -41,6 +42,14 @@ SnowPlowVehicle::SnowPlowVehicle() {
     
     // get the offset from the vehicles X, Y, and Z
     this->getOffsets();
+    
+    // set the front wheels and rims to steer
+    LFwheel->isSteering = true;
+    RFwheel->isSteering = true;
+    LFrim->isSteering = true;
+    RFrim->isSteering = true;
+    
+    //
     
     body->draw();
     frontdiffuser->draw();
@@ -77,6 +86,7 @@ SnowPlowVehicle::SnowPlowVehicle(VehicleModel vm) {
                 // just a pointer to the rectangular parameters struct so I don't have to type it all out
                 struct ShapeParameter::RectangularParameters* data = &(it->params.rect);
                 RectangularPrism* rect = new RectangularPrism(it->xyz[0], it->xyz[0], it->xyz[0], data->xlen, data->ylen, data->zlen, it->rotation);
+                rect->setColor(it->rgb[0], it->rgb[1], it->rgb[2]);
                 rect->draw();
                 this->addShape(rect);
                 break;
@@ -84,22 +94,27 @@ SnowPlowVehicle::SnowPlowVehicle(VehicleModel vm) {
                 
             case TRIANGULAR_PRISM: {
                 struct ShapeParameter::TriangularParameters* data = &(it->params.tri);
-                float v1[] = {1.5f, 2.f, -2.f};
-                float v2[] = {0.f, 3.5f, -2.f};
-                float v3[] = {-2.f, 3.5f, -2.f};
+                float v1[] = {it->xyz[0], it->xyz[1], it->xyz[2]};
+                float v2[] = {static_cast<float>((it->xyz[0] - data->blen*cos(data->angle*M_PI/180.f))), static_cast<float>((it->xyz[1] + data->blen*sin(data->angle*M_PI/180.f))), it->xyz[2]};
+                float v3[] = {it->xyz[0] + data->alen, it->xyz[1], it->xyz[2]};
                 TriangularPrism* tri = new TriangularPrism(v1, v2, v3, data->depth, it->rotation);
+                tri->setColor(it->rgb[0], it->rgb[1], it->rgb[2]);
                 tri->draw();
                 this->addShape(tri);
                 break;
             }
                 
             case TRAPEZOIDAL_PRISM: {
-                
+                std::cout << "Can't be bothered to actually make one" << std::endl;
                 break;
             }
                 
             case CYLINDER: {
-                
+                struct ShapeParameter::CylinderParameters* data = &(it->params.cyl);
+                Cylinder* cyl = new Cylinder(it->xyz[0], it->xyz[0], it->xyz[0], data->radius, data->depth, it->rotation);
+                cyl->setColor(it->rgb[0], it->rgb[1], it->rgb[2]);
+                cyl->draw();
+                this->addShape(cyl);
                 break;
             }
                 
@@ -123,12 +138,13 @@ void SnowPlowVehicle::draw() {
     // all components to the correct rotation and position
     this->positionInGL();
     
-    // add steering
-    LFwheel->setRotation(steering);
-    RFwheel->setRotation(steering);
+    
     
     for (it = shapes.begin(); it != shapes.end(); ++it) {
-        
+        // add steering
+        if ((*it)->isSteering) {
+            (*it)->setRotation(steering);
+        }
         (*it)->draw();
     }
     
